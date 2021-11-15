393201d2e653349b97452914a2f66add4a905223
daudi44 <daudi@iesebre.com>
2021-11-15 19:25:49 +0100

    Versió amb Single File Components

---
+++ vue_v1/counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{{counter}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data() {
+    return{
+    counter: 0
+  }
+},
+  methods:{
+    increment(){
+      this.counter++
+    },
+    decrement(){
+      this.counter--
+    }
+  }
+}
+</script>
